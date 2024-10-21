8a1cc36e82799729ce8026e7bf49fa7382b64789
Hassan Ahmad <hassan.ahmad@mergestack.com>
2024-10-21 19:57:33 +0500

    Worked Feedback Changes

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,4 +1,4 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
 import taskListReducer from "store/reducers/taskListSlice";
 
@@ -10,5 +10,5 @@ const store = configureStore({
 export default store;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch = useDispatch.withTypes<AppDispatch>()
-export const useAppSelector = useSelector.withTypes<RootState>()
+export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
+export const useAppSelector = useSelector.withTypes<RootState>();
