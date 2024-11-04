93e52f8e195c1187a94fd4249ea1bd6ee4cae680
Hassan Ahmad <hassan.ahmad@mergestack.com>
2024-11-04 20:00:41 +0500

    A minor logical improvement

--- src/containers/LayoutContainer/index.ts
+++ src/containers/LayoutContainer/index.ts
@@ -1,4 +1,4 @@
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "store/store";
 import Layout from "components/Layout";
 import { ILayoutContainerProps } from "type";
@@ -8,7 +8,7 @@ const mapStateToProps = (
   state: RootState,
   ownProps: ILayoutContainerProps
 ) => ({
-  isAuthenticated: new Boolean(state.user._id).valueOf(),
+  isAuthenticated: state.user.isAuthenticated,
   children: ownProps.children,
 });
 
